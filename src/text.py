289d1d314a129dd9f5bddea94923b2901a716602
sleepy0owl <souravcristiano502@gmail.com>
2022-06-28 14:00:13 +0530

    Add code to retrieve file from s3

--- src/text.py
+++ src/text.py
@@ -1,4 +1,27 @@
+import json
 import boto3
 
 def handler(event, context):
-    print(event)
+    try:
+        record = event['Records'][0]
+        bucket_name = record['s3']['bucket']['name']
+        key = record['s3']['object']['key']
+
+        s3_client = boto3.client('s3')
+        response = s3_client.get_object(
+            Bucket=bucket_name,
+            Key=key
+        )
+        data = response['Body'].read().decode('utf-8')
+        # call comprehend endpoint
+
+        return {
+            "statusCode": 200,
+            "body": json.dumps({"message": "success"})
+        }
+    except Exception as e:
+        print(e)
+        return {
+            "statusCode": 500,
+            "body": json.dumps({"message": "Internal server error"})
+        }
